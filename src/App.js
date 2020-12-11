6a6ce5bbe29c6ea699c0b74e84725adee829bbf8
fehol <felix.hollmann@gmx.net>
2020-12-11 15:05:11 +0100

    Chrismas Card ready

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="main">
-      <div className="snow"></div>
+    <div className="container">
+      <h1 className="animated">Frohe Weihnachten</h1>
+      <p className="Text">
+        und herzlichen Dank für die erfolgreiche Zusammenarbeit in 2020.
+        <br />
+        Das Team der Kanzlei Breitmeyer freut sich, mit Ihnen in das neue Jahr
+        zu starten.
+      </p>
+      <img
+        src="https://media.giphy.com/media/f5w18q4nzplKT0UcNH/giphy.gif"
+        alt="Rudolf"
+      />
     </div>
   );
 }
